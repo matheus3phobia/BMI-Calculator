94e187fb486f2abcb8120650ff4ca968fe4fc257
Matheus Henrique <matheus.hperes@hotmail.com>
2022-06-02 04:59:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import styles from "./App.module.css";
+import poweredImage from "./assets/powered.png";
+import leftArrow from "./assets/left-arrow.png";
+import { useState } from "react";
+import { levels, calculateIMC, Level } from "./helpers/imc";
+import { GridItem } from "./components/GridItem";
+
+function App() {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
+
+  const handleCalculator = () => {
+    if (heightField && weightField) {
+      setToShow(calculateIMC(heightField, weightField));
+    } else {
+      alert("Por favor preencha os campos corretamente");
+    }
+  };
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
+  };
+
+  return (
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="Powered by b7web" width={150} />
+        </div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC</h1>
+          <p>
+            IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela
+            Organização Mundial de Saúde para calcular o peso ideal de cada
+            pessoa.
+          </p>
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex: 1.7 (em metros)"
+            value={heightField > 0 ? heightField : ""}
+            onChange={(event) => setHeightField(parseFloat(event.target.value))}
+            disabled={toShow ? true : false}
+          />
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex: 70 (em quilos)"
+            value={weightField > 0 ? weightField : ""}
+            onChange={(event) => setWeightField(parseFloat(event.target.value))}
+            disabled={toShow ? true : false}
+          />
+          <button onClick={handleCalculator} disabled={toShow ? true : false}>
+            Calcular
+          </button>
+        </div>
+        <div className={styles.rightSide}>
+          {!toShow && (
+            <div className={styles.grid}>
+              {levels.map((item, key) => (
+                <GridItem key={key} item={item} />
+              ))}
+            </div>
+          )}
+          {toShow && (
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrow} alt="" width={25} />
+              </div>
+              <GridItem item={toShow} />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/helpers/imc.ts
@@ -0,0 +1,28 @@
+export type Level = {
+  title: string;
+  color: string;
+  icon: "down" | "up";
+  imc: number[];
+  yourIMC?: number;
+};
+
+export const levels = [
+  { title: "Magreza", color: "#97a3ab", icon: "down", imc: [0.1, 18.5] },
+  { title: "Normal", color: "#0ead69", icon: "up", imc: [18.6, 24.9] },
+  { title: "Sobrepeso", color: "#e2b036", icon: "down", imc: [25, 30] },
+  { title: "Obesidade", color: "#c3423f", icon: "down", imc: [30.1, 99] },
+];
+
+export const calculateIMC = (weight: number, height: number) => {
+  const imc = weight / (height * height);
+
+  for (let i in levels) {
+    if (imc >= levels[i].imc[0] && imc <= levels[i].imc[1]) {
+      let levelCopy: Level = {...levels[i]};
+      levelCopy.yourIMC = parseFloat(imc.toFixed(2));
+      return levels[i];
+    }
+  }
+
+  return null;
+};
